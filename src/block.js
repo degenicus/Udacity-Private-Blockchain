e4000fb312d0088eecf73b0823cf1ef56c160998
Magnus <magnus@localhost.localdomain>
2021-07-31 17:54:22 +0200

    Implemented Block class

--- src/block.js
+++ src/block.js
@@ -15,10 +15,10 @@ const hex2ascii = require('hex2ascii');
 class Block {
 
     // Constructor - argument data will be the object containing the transaction data
-	constructor(data){
+	constructor(data) {
 		this.hash = null;                                           // Hash of the block
 		this.height = 0;                                            // Block Height (consecutive number of each block)
-		this.body = Buffer.from(JSON.stringify(data)).toString('hex');   // Will contain the transactions stored in the block, by default it will encode the data
+		this.body = this._toHex(data);   // Will contain the transactions stored in the block, by default it will encode the data
 		this.time = 0;                                              // Timestamp for the Block creation
 		this.previousBlockHash = null;                              // Reference to the previous Block Hash
     }
@@ -38,14 +38,25 @@ class Block {
     validate() {
         let self = this;
         return new Promise((resolve, reject) => {
-            // Save in auxiliary variable the current block hash
-                                            
-            // Recalculate the hash of the Block
-            // Comparing if the hashes changed
-            // Returning the Block is not valid
-            
-            // Returning the Block is valid
-
+            try {
+                // Save in auxiliary variable the current block hash
+                const blockHash = self.hash;                                
+                // Copy block to avoid data mutation
+                const blockCopy = JSON.parse(JSON.stringify(self));
+                // Recalculate the hash of the Block
+                blockCopy.hash = '';
+                const currentHash = SHA256(JSON.stringify(blockCopy)).toString();
+                // Comparing if the hashes changed
+                if (blockHash !== currentHash) {
+                    // Returning the Block is not valid
+                    resolve(false);
+                } else {
+                    // Returning the Block is valid
+                    resolve(true);
+                }
+            } catch (error) {
+                this._handleError(error, "validate", reject);
+            }
         });
     }
 
@@ -59,14 +70,45 @@ class Block {
      *     or Reject with an error.
      */
     getBData() {
-        // Getting the encoded data saved in the Block
-        // Decoding the data to retrieve the JSON representation of the object
-        // Parse the data to an object to be retrieve.
-
-        // Resolve with the data if the object isn't the Genesis block
-
+        let self = this;
+        return new Promise(async (resolve, reject) => {
+            try {
+                // Getting the encoded data saved in the Block
+                // Decoding the data to retrieve the JSON representation of the object
+                const bodyASCII = hex2ascii(this.body);
+                // Parse the data to an object to be retrieve.
+                const body = JSON.parse(bodyASCII);
+                // Resolve with the data if the object isn't the Genesis block
+                if (!self._isGenesisBlock()) {
+                    resolve(body);
+                } else {
+                    reject("Cannot fetch the genesis block data");
+                }
+            } catch (error) {
+                self._handleError(error, "getBData", reject);
+            }
+            
+        });
     }
 
+    _toHex(data) {
+        return Buffer.from(JSON.stringify(data)).toString('hex');
+    }   
+
+    _isGenesisBlock() {
+        const body = this.body;
+        const genesisHex = this._toHex({data: 'Genesis Block'});
+        if (body && typeof body === 'string' && body === genesisHex) {
+            return true;
+        }
+        return false;
+    }
+
+    _handleError(error, methodName, reject) {
+        const errorMessage = `${methodName} threw an error: ${JSON.stringify(error)}`;
+        console.log(errorMessage);
+        reject(errorMessage);
+    }
 }
 
 module.exports.Block = Block;                    // Exposing the Block class as a module
